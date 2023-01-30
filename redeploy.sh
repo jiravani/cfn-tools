db4f2919f5fb965db746c911c1748b62f88bce5b
Justin Iravani <5256796+jiravani@users.noreply.github.com>
2023-01-30 16:50:44 -0600

    Corrected variables names

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 templatefile="template.yaml"
-awsprofile="<AWS Profile>"
-s3bucket="<S3 Bucket>"
+awsprofile="jiravani-Admin"
+s3bucket="aws-jiravani-us-east-2"
 username=$(echo "${USER//./}")
 stackname=$username-stack-$(date +"%Y-%m-%d")
+parameterfile=''
 
 while [ "$1" != "" ]; do
     case "$1" in
@@ -32,7 +33,7 @@ while [ "$1" != "" ]; do
         shift
         parameterfile=$1
         ;;
-    -h | --help)
+    -h | --help | *)
         shift
         help=1
         ;;
@@ -65,43 +66,49 @@ if [ "$help" = "1" ]; then
     exit
 fi;
 
-[ ! -f ./$templatefile ] && echo "The expected template file $templatefile could not be found. Please specify your CloudFormation template file with -f or --template-file." && exit   
+[ ! -f ./$templatefile ] && echo "The expected template file ${templatefile} could not be found. Please specify your CloudFormation template file with -f or --template-file." && exit   
 
 
-if [ "$deletestack" = "1" ]; then
-    echo "Deleting Cloudformation stack $stackname"
-    aws cloudformation delete-stack --stack-name "$stackname" --profile "$awsprofile"
-    aws cloudformation wait stack-delete-complete --stack-name "$stackname"
-    echo "$stackname was deleted."
-    if [ "$teardown" = "1" ];then
+if [ "${deletestack}" = "1" ]; then
+    echo "Deleting Cloudformation stack ${stackname}"
+    aws cloudformation delete-stack --stack-name "${stackname}" --profile "${awsprofile}"
+    aws cloudformation wait stack-delete-complete --stack-name "${stackname}"
+    echo "${stackname} was deleted."
+    if [ "${teardown}" = "1" ];then
         exit
     fi;
 fi;  
     
-aws cloudformation package  --template-file "$templatefile" \
-                            --output-template-file "packaged-$templatefile" \
-                            --s3-bucket "$s3bucket" --profile "$awsprofile" > /dev/null
+aws cloudformation package  --template-file "${templatefile}" \
+                            --output-template-file "packaged-${templatefile}" \
+                            --s3-bucket "${s3bucket}" --profile "${awsprofile}" > /dev/null
 
-echo "Successfully packaged artifacts and wrote output template to file packaged-$templatefile."
-if [ -z "$parameters" ];
+echo "Successfully packaged artifacts and wrote output template to file packaged-${templatefile}."
+
+# echo ${parameterfile}
+if [[ -z "${parameterfile}" ]];
 then  
     aws cloudformation deploy --template "packaged-$templatefile" \
     --capabilities CAPABILITY_NAMED_IAM \
-    --stack-name "$stackname" \
-    --profile "$awsprofile" \
-    --parameter-overrides "file://./$parameterfile" 
-    
+    --stack-name "${stackname}" \
+    --profile "${awsprofile}"
+
+    aws cloudformation wait stack-create-complete --stack-name "$stackname"
+
     aws cloudformation describe-stack-events --stack-name "$stackname" \
     --query 'StackEvents[].[Timestamp, ResourceStatus, LogicalResourceId, ResourceStatusReason]' \
     --output table
 
 else
-    aws cloudformation deploy --template "packaged-$templatefile" \
+    aws cloudformation deploy --template "packaged-${templatefile}" \
     --capabilities CAPABILITY_NAMED_IAM \
-    --stack-name "$stackname" \
-    --profile "$awsprofile" 
-    aws cloudformation describe-stack-events --stack-name "$stackname" \
+    --stack-name "${stackname}" \
+    --profile "${awsprofile}" \
+    --parameter-overrides "file://./${parameterfile}"
+
+    aws cloudformation wait stack-create-complete --stack-name "${stackname}"
+
+    aws cloudformation describe-stack-events --stack-name "${stackname}" \
     --query 'StackEvents[].[Timestamp, ResourceStatus, LogicalResourceId, ResourceStatusReason]' \
     --output table
 fi;
-
